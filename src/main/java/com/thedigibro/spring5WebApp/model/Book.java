63bbcbf78bb9f6b377b6e426c6904b9f44c59abe
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-27 15:17:18 +0200

    H2 data model created.

--- src/main/java/com/thedigibro/spring5WebApp/model/Book.java
+++ src/main/java/com/thedigibro/spring5WebApp/model/Book.java
@@ -10,12 +10,16 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String title;
     private String isbn;
     private String publisher;
 
     @ManyToMany
+    @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors= new HashSet<>();
+
+    public Book(){}
 
     public Book(String title,String isbn,String publisher) {
         this.title=title;
